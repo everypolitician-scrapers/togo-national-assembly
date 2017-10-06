dee36025ea90026e0872ea5e724f176ecf7f41c0
Tony Bowden <tony@tmtm.com>
2017-10-06 09:48:22 +0100

    Show debug output if MORPH_DEBUG is set

--- scraper.rb
+++ scraper.rb
@@ -43,6 +43,7 @@ def scrape_list(url)
       gender:           gender_from(prefix),
       term:             2013,
     }
+    puts data.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h if ENV['MORPH_DEBUG']
     ScraperWiki.save_sqlite(%i[name term], data)
   end
 end
