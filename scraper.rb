ea83e5508b4885a65a75b2d84444d4c7812f7619
Tony Bowden <tony@tmtm.com>
2019-02-08 10:49:53 +0000

    Update for 2018 term

--- scraper.rb
+++ scraper.rb
@@ -16,62 +16,35 @@ end
 
 class MembersList < Scraped::HTML
   field :members do
-    noko.css('#jsn-mainbody table tbody tr').map { |td| fragment(td => Member).to_h }
+    noko.xpath('.//table[.//td[contains(.,"CIRCONSCRIPTION")]]//tr').drop(1).map { |tr| fragment(tr => Member).to_h }
   end
 end
 
 class Member < Scraped::HTML
   field :id do
-    fullname.idify
+    name.idify
   end
 
   field :name do
-    nameparts.last
-  end
-
-  field :honorific_prefix do
-    nameparts.first
-  end
-
-  field :sort_name do
-    name
-  end
-
-  field :party do
-    tds[1].text.strip
+    tds[0].text.tidy
   end
 
   field :area do
-    tds[2].text.strip
+    tds[1].text.tidy
   end
 
-  field :gender do
-    return 'female' if honorific_prefix == 'Mme'
-    return 'male'   if honorific_prefix == 'M'
-
-    raise "Unknown gender for #{honorific_prefix}"
+  field :party do
+    tds[2].text.tidy
   end
 
   field :term do
-    2013
+    2018
   end
 
   private
 
   def tds
     noko.css('td')
-  end
-
-  def fullname
-    tds[0].text.gsub(/[[:space:]]+/, ' ').strip
-  end
-
-  def nameparts
-    name = fullname.dup
-    return ['Mme', name] if name.sub! /^Mme\.?\s/, ''
-    return ['M', name] if name.sub! /^M[\. ]+/, ''
-
-    return
   end
 end
 
